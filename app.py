c8bc8a9c9fed60e731e078d1d3331c982bdb9d47
Joe Downard <joehdownard@gmail.com>
2020-07-05 23:48:07 +0100

    users can register now

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify, request
+import os
+
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -13,11 +17,12 @@ def get_data():  # get data every time you request so it's updated
 
 
 def get_new_user_id():
-    file = open('./data/user.json', 'r')
-    data = json.load(file)
-    # file.close()
-    latest_id = data[-1]['userID']
-    return latest_id + 1
+    if os.path.isfile('./data/user.json'):
+        with open('./data/user.json') as f:
+            data = json.load(f)
+            return data[len(data)-1][0]['userID'] + 1
+    else:
+        return 0
 
 
 @app.route('/')
@@ -56,21 +61,49 @@ def get_stock_value():
             return str(stock['value'])
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'OPTIONS'])
 def register():
-    if 'username' not in request.args or 'password' not in request.args:
-        return "invalid"
-    username = request.args['username']
-    password = request.args['password']
-    user_id = get_new_user_id()
-    data = [{
-        'userID': user_id,
-        'username': username,
-        'password': password,
-    }]
-    json.dump(data, open('./data/user.json', 'w'))
+    if request.method == "OPTIONS":
+        return _build_cors_prelight_response()
+    elif request.method == "POST":
+        print(request.json['username'])
+        if 'username' not in request.json or 'password' not in request.json or 'email' not in request.json:
+            return "invalid"
+        username = request.json['username']
+        password = request.json['password']
+        email = request.json['email']
+        user_id = get_new_user_id()
+        data = [{
+            'userID': user_id,
+            'username': username,
+            'email': email,
+            'password': password,
+        }]
+        a = []
+        if not os.path.isfile('./data/user.json'):
+            a.append(data)
+            with open('./data/user.json', mode='w') as f:
+                f.write(json.dumps(a, indent=1))
+        else:
+            with open('./data/user.json') as f:
+                jsonObject = json.load(f)
+
+            jsonObject.append(data)
+            with open('./data/user.json', mode='w') as f:
+                f.write(json.dumps(jsonObject, indent=1))
+        response = jsonify("piss")
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+    app.run(port=5000)
 #
